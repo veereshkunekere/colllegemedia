851e894f4fee17e06438e7c509d7d097ebbc81aa
veeresh <veeresh312004@gmail.com>
2026-01-05 23:04:55 +0530

    added socket cors render frontend url

--- backend/controllers/socketManager.js
+++ backend/controllers/socketManager.js
@@ -5,7 +5,7 @@ const onlineUsers = new Map(); // userId => { sockets: [], timeout: null }
 const socketManager = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: 'http://localhost:5173',
+            origin: 'https://colllegemedia-froontend.onrender.com',
             methods: ['GET', 'POST'],
             credentials: true,
         },
